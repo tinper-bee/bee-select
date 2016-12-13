422207655980eda9e2ea1a1d6b9bdf52f9d85bd7
ahua52 <1468492018@qq.com>
2016-12-13 11:30:03 +0800

    修改依赖包

--- test/Select.test.js
+++ test/Select.test.js
@@ -29,6 +29,49 @@ describe('Select single test', function() {
 		let select = mount(<Select size="sm" defaultValue="lucy" style={{ width: 200 }} onChange={this.handleChange}></Select>);
 		expect(select.find('.u-select-sm').length).to.equal(1);
 	})
+
+	it('Option Select should be exist', function() {
+		class Demo1 extends Component {
+			constructor() {
+				super();
+				this.handleChange = this.handleChange.bind(this);
+			}
+			handleChange(value) {
+		  		console.log(`selected ${value}`);
+			}
+			render(){	
+				return( 
+					<div>
+					    <Select size="lg" defaultValue="lucy" style={{ width: 200 }} onChange={this.handleChange}>
+					      <Option value="jack">Jack</Option>
+					      <Option value="lucy">Lucy</Option>
+					      <Option value="disabled" disabled>Disabled</Option>
+					      <Option value="yiminghe">Yiminghe</Option>
+					    </Select>
+					    <Select defaultValue="lucy" style={{ width: 200 }} onChange={this.handleChange}>
+					      <Option value="jack">Jack</Option>
+					      <Option value="lucy">Lucy</Option>
+					      <Option value="disabled" disabled>Disabled</Option>
+					      <Option value="yiminghe">Yiminghe</Option>
+					    </Select>
+					    <Select size="sm" defaultValue="lucy" style={{ width: 200 }} onChange={this.handleChange}>
+					      <Option value="jack">Jack</Option>
+					      <Option value="lucy">Lucy</Option>
+					      <Option value="disabled" disabled>Disabled</Option>
+					      <Option value="yiminghe">Yiminghe</Option>
+					    </Select>
+					</div>
+				)
+			}
+		}
+		let select = mount(<Demo1 />, { attachTo: document.body});
+		let selCli = select.find('.u-select').at(0);
+
+		selCli.simulate("click");
+		let option = window.document.querySelector('.u-select-dropdown');
+		console.log(option);
+		//expect(option.length).to.equal(1);
+	})
 	
 	
 	
