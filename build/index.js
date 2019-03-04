77fc5959483ed93130e6b6ec9c718cde112ed8b3
liushaozhen <804199994@qq.com>
2019-03-04 20:01:43 +0800

    fix(bee-select): export 问题

--- build/index.js
+++ build/index.js
@@ -3,7 +3,6 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.SelectPropTypes = exports.OptGroup = exports.Option = undefined;
 
 var _Select = require('./Select');
 
@@ -23,7 +22,5 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 _Select2["default"].Option = _Option2["default"];
 _Select2["default"].OptGroup = _OptGroup2["default"];
-exports.Option = _Option2["default"];
-exports.OptGroup = _OptGroup2["default"];
-exports.SelectPropTypes = _PropTypes.SelectPropTypes;
 exports["default"] = _Select2["default"];
+module.exports = exports['default'];
