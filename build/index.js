6cc2167b7953456cd497edcd85119f5f6938796d
“jonyshi” <jonyshi@126.com>
2019-03-05 15:13:31 +0800

    version 修改

--- build/index.js
+++ build/index.js
@@ -4,8 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 var _Select = require('./Select');
 
 var _Select2 = _interopRequireDefault(_Select);
@@ -22,7 +20,7 @@ var _OptGroup2 = _interopRequireDefault(_OptGroup);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-_Select2["default"].Option = _extends({}, _Option2["default"]);
-_Select2["default"].OptGroup = _extends({}, _OptGroup2["default"]);
+_Select2["default"].Option = _Option2["default"];
+_Select2["default"].OptGroup = _OptGroup2["default"];
 exports["default"] = _Select2["default"];
 module.exports = exports['default'];
